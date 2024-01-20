228f7b560c453418a1d90c17d077e3d12fcfadcc
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-01-20 12:59:58 +0300

    new3

--- inputs/src/App.js
+++ inputs/src/App.js
@@ -32,6 +32,7 @@ const mouseText = [
 function App() {
   const [arrObj, setArrObj] = useState(mouseText);
   const [obj, setObj] = useState( getEmptyObj() );
+  const [idToEdit, setIdToEdi] = useState(null);
 
   function add() {
     setArrObj([...arrObj, obj]);
@@ -46,12 +47,35 @@ function App() {
     setArrObj(arrObj.filter( obj => obj.id != id ));
   }
 
+  function getValue(prop) {
+    arrObj.reduce( (res, obj) => {
+      if (obj.id == idToEdit) {
+        return obj[prop];
+      } else {
+        return res;
+      }
+    } );
+  }
+
+  function change(prop, event) {
+    setArrObj( arrObj.map( obj => {
+      if (obj.id == idToEdit) {
+        return {...obj, [prop]:event.target.value};
+      } else {
+        return obj;
+      }
+    } ) );
+  }
+
   const result = arrObj.map( obj => {
     return <p key={obj.id}>
       { obj.prop1 } { obj.prop2 } { obj.prop3 }
+      <br/>
       <button onClick={() => delObj(obj.id)}>удалить объект</button>
+      <br/>
+      <button onClick={() => getValue(obj.id)}>редактировать текущий объект</button>
     </p>
-  } )
+  } );
 
   return (
   <div className={classes.app}>
@@ -62,12 +86,18 @@ function App() {
     <br/>
     {result}
     <br/>
+    <div>ввод данных нового оъекта</div>
     <input value={obj.prop1} onChange={event => change('prop1', event)} />
     <input value={obj.prop2} onChange={event => change('prop2', event)} />
     <input value={obj.prop3} onChange={event => change('prop3', event)} />
     <br/>
     <button onClick={add}>добвить объект</button>
-    {console.log(arrObj)}
+    <br/>
+    <br/>
+    <div>редактирование данных существующего объекта</div>
+    <input value={getValue('prop1')} onChange={event => change('prop1', event)} />
+    <input value={getValue('prop2')} onChange={event => change('prop2', event)} />
+    <input value={getValue('prop3')} onChange={event => change('prop3', event)} />
   </div>
   );
 }
