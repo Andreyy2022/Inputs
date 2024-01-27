52adf42c4b811fa939a7b863f8e7c4e33f457f61
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-01-27 12:15:09 +0300

    5

--- inputs/src/App.js
+++ inputs/src/App.js
@@ -5,14 +5,24 @@ import { nanoid } from 'nanoid';
 
 function App() {
   const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
+  const [value, setValue] = useState('');
+  const [indexValue, setIndexValue] = useState('');
 
-  function double(index, note) {
-    setNotes( [...notes.slice(0, index), note ** 2, ...notes.slice(index + 1)] );
+  function valueIndex(note, index) {
+    setValue(note);
+    setIndexValue(index);
+  }
+
+  function replace(index) {
+    setNotes([...notes.slice(0, index), value, ...notes.slice(index + 1)]);
+    console.log(notes);
+    console.log(value);
+    console.log(indexValue);
   }
 
   const result = notes.map((note, index) => {
     return <div key={index}>
-        <li onClick={() => double(index, note)}>
+        <li onClick={(note, index) => valueIndex(note, index)}>
           {note}
         </li>
         <button onClick={() => setNotes([...notes.slice(0, index), ...notes.slice(index + 1)])}>delete</button>
@@ -28,6 +38,7 @@ function App() {
       <ul>
         {result}
       </ul>
+      <input value={value} onChange={(event) => setValue(event.target.value)} onBlur={indexValue => replace(indexValue)} />
   </div>
   );
 }
